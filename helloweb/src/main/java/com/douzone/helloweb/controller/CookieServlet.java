8daa8bb7464ee6edeb8b736915dbb5f6aec8fc13
BIT <BIT@DESKTOP-U4THS2M>
2021-05-28 11:10:27 +0900

    Cookie 연습

--- helloweb/src/main/java/com/douzone/helloweb/controller/CookieServlet.java
+++ helloweb/src/main/java/com/douzone/helloweb/controller/CookieServlet.java
@@ -12,15 +12,15 @@ import javax.servlet.http.HttpServletResponse;
 public class CookieServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String COOKIE_NAME ="visitCount";
-	int visitCount = 0;
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int visitCount = 0;
 		
 		//쿠키 읽기
 		Cookie[] cookies = request.getCookies();
 		if(cookies != null && cookies.length > 0) {
 			for(Cookie cookie : cookies) {
-				if("COOKIE_NAME".equals(cookie.getName())) {
+				if(COOKIE_NAME.equals(cookie.getName())) {
 					visitCount = Integer.parseInt(cookie.getValue());
 				}
 			}
